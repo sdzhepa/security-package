e776329ee60b3a4e1867835eabe5dc7a0b709e23
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-02-18 14:53:33 +0200

    security-package/issues/110: Revert changes.

--- ReCaptchaUser/Observer/LoginObserver.php
+++ ReCaptchaUser/Observer/LoginObserver.php
@@ -88,10 +88,6 @@ class LoginObserver implements ObserverInterface
         if ($this->captchaConfig->isCaptchaEnabledFor('user_login')
             && $this->request->getFullActionName() === $this->loginActionName
         ) {
-
-
-            echo 'prfllrfppfelf';
-
             $reCaptchaResponse = $this->request->getParam(
                 CaptchaValidatorInterface::PARAM_RECAPTCHA_RESPONSE
             );
